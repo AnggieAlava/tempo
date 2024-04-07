a8b69fba82281fa9e60e9efa120b07fbdf74bbaa
Anggie Alava <codewithangels@gmail.com>
2024-04-07 00:33:58 -0500

    index.jsx movido a root

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
+import App from './src/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
